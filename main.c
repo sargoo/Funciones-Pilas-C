1be9140225458de918e82131c73cac0989102f96
sargoo <76232198+sargoo@users.noreply.github.com>
2022-05-30 12:36:57 -0300

    Ejercicio 8 terminado

--- main.c
+++ main.c
@@ -3,402 +3,253 @@
 #include "pila.h"
 #include <conio.h>
 #include <time.h>
+
+#define DIM 50
 #define ESC 27
-#define ANCHO 118
 
-void contarTitulo(char titulo[]);
-void cargarPila (Pila*p);
-void pasarPila (Pila *p, Pila *p2);
-void pasarPilaIgual (Pila *p, Pila *p2);
-int encontrarMenor (Pila *p);
-void apilarOrdenado(Pila *p);
-void insertarElementoEnOrdenada(Pila *p);
-void imprimirPila(Pila p);
-void cargarPilaRandom(Pila *p, int lim);
-void pasarPilaDeMenorAMayor (Pila *p, Pila *p2);
-int sumarElementos (Pila p);
 
-void main()
+int cargarArreglo (int arreglo[], int valid);
+void mostrarArreglo (int arreglo[], int valid);
+void sumaElementos (int arreglo[], int valid);
+void copiarElementos(int arreglo[], int valid, Pila *p);
+int  cargarRandom(float arreglo[], int dimension, int valid);
+void mostrarFloat (float arreglo[], int valid);
+int encontrarCaracter (char arreglo[], int valid, char caracter);
+int cargarArregloCaracter (char arreglo[], int valid);
+int insertarOrdenado(char a[], int v, char caracter);
+void mostrarChar (char arreglo[], int valid);
+char encontrarMayor (char a[], int valid);
+
+int main()
 {
-    int opcion = 0, ejercicio = 0, topeP, siguienteTope, suma;
-    Pila pila, otra, ordenada;
-    inicpila(&pila);
-    inicpila(&otra);
     srand(time(NULL));
-    int limite = rand()%(11-5) + 5;
-    int menor = 0;
+    int array[DIM];
+    int flag = 0;
+    char arrayCaracteresOrdenado [DIM];
+    char arrayCaracteres [DIM];
+    char otroArray [DIM];
+    float arrayFloat [100];
+    int validos = 0;
+    float validosFloat = 0;
+    int validosCaracteres = 0;
+    char buscar;
+    char mayor;
+    Pila pila;
+    inicpila(&pila);
 
+    validos = cargarArreglo (array, validos);
+    printf("Validos : %i\n", validos);
+    mostrarArreglo (array, validos);
+    sumaElementos ( array, validos);
+    copiarElementos(array, validos, &pila);
+    validosFloat = cargarRandom(arrayFloat, 100, validos);
+    mostrarFloat(arrayFloat, validosFloat);
 
-    while (opcion != ESC)
+    validosCaracteres = cargarArregloCaracter (arrayCaracteresOrdenado, validosCaracteres);
+    printf("\n Indique que caracter quiere buscar en el arreglo: ");
+    fflush(stdin);
+    scanf("%c", &buscar);
+    system("cls");
+    flag = encontrarCaracter(arrayCaracteresOrdenado, validosCaracteres, buscar);
+
+    if(flag == 1)
+
     {
-
-        imprimirTecho(ANCHO);
-        contarTitulo("GUIA PRACTICA 3 - FUNCIONES");
-        imprimirTecho(ANCHO);
-        contarTitulo("INGRESE EL NUMERO DEL EJERCICIO QUE DESEA EJECUTAR");
-        imprimirTecho(ANCHO);
-        contarTitulo("1- Hacer una funcion que permita ingresar varios elementos a una pila, tanto como quiera el usuario.");
-        imprimirTecho(ANCHO);
-        contarTitulo("2- Hacer una funcion que pase todos los elementos de una pila a otra.");
-        imprimirTecho(ANCHO);
-        contarTitulo("3- Hacer una funcion que pase todos los elementos de una pila a otra, pero conservando el orden.");
-        imprimirTecho(ANCHO);
-        contarTitulo("4- Hacer una funcion que encuentre el menor elemento de una pila y lo retorna.");
-        contarTitulo("La misma debe eliminar ese dato de la pila.");
-        imprimirTecho(ANCHO);
-        contarTitulo("5-Hacer una funcion que pase los elementos de una pila a otra, de manera que se genere una nueva pila ordenada.");
-        contarTitulo("Usar la funcion del ejercicio 4.");
-        imprimirTecho(ANCHO);
-        contarTitulo("6- Hacer una funcion que inserta en una pila ordenada un nuevo elemento, conservando el orden de esta.");
-        imprimirTecho(ANCHO);
-        contarTitulo("7- Hacer una funci贸n que pase los elementos de una pila a otra, de manera que se genere una nueva pila ordenada.");
-        contarTitulo("Usar la funci贸n del ejercicio 6.  (Ordenamiento por inserci贸n).");
-        imprimirTecho(ANCHO);
-        contarTitulo("8- Hacer una funcion que sume y retorne los dos primeros elementos de una pila (tope y anterior),");
-        contarTitulo(" sin alterar su contenido. ");
-        imprimirTecho(ANCHO);
-        contarTitulo("9- Hacer una funcion que calcule el promedio de los elementos de una pila,");
-        contarTitulo("para ello hacer tambien una funcion que calcule la suma, otra para la cuenta y otra que divida.");
-        contarTitulo("En total son cuatro funciones, y la funci贸n que calcula el promedio invoca a las otras 3. ");
-        imprimirTecho(ANCHO);
-        contarTitulo("10- Hacer una funcion que reciba una pila con numeros de un solo digito,");
-        contarTitulo("(es responsabilidad de quien usa el programa) y que transforme esos d铆gitos en un numero decimal");
-        imprimirTecho(ANCHO);
-        scanf("%i", &ejercicio);
-        system("cls");
-        switch(ejercicio)
-        {
-        case 1:
-            contarTitulo("1-Hacer una funcion que permita ingresar varios elementos a una pila, tanto como quiera el usuario. ");
-            cargarPila (&pila);
-            imprimirPila(pila);
-            break;
-
-        case 2:
-            contarTitulo("2-Hacer una funcion que pase todos los elementos de una pila a otra. \n");
-
-            cargarPilaRandom(&pila, limite);
-            ///int superior = tope(&pila);
-            contarTitulo("PILA: ");
-            imprimirPila(pila);
-            pasarPila (&pila, &otra);
-            contarTitulo("OTRA PILA: ");
-            imprimirPila(otra);
-            pasarPila (&otra, &pila);
-            break;
-
-        case 3:
-            contarTitulo("3-Hacer una funcion que pase todos los elementos de una pila a otra, pero conservando el orden.\n");
-            cargarPilaRandom(&pila, limite);
-            contarTitulo("PILA: ");
-            imprimirPila(pila);
-            pasarPilaIgual (&pila, &otra);
-            contarTitulo("OTRA: ");
-            imprimirPila(pila);
-            break;
-
-        case 4:
-            contarTitulo("4. Hacer una funcion que encuentre el menor elemento de una pila y lo retorna.");
-            contarTitulo("La misma debe eliminar ese dato de la pila.");
-            cargarPilaRandom(&pila, limite);
-            imprimirPila(pila);
-            menor = encontrarMenor (&pila);
-            contarTitulo("EL elemento menor de la pila es: ");
-            printf("\t\t\t\t\t\t\t%i\n", menor);
-            contarTitulo("PILA SIN EL MENOR ELEMENTO: ");
-            imprimirPila(pila);
-
-            break;
-
-        case 5:
-            contarTitulo("Hacer una funcion que pase los elementos de una pila a otra, de manera que se genere una nueva pila ordenada. Usar la funcion del ejercicio 4. (Ordenamiento por seleccion\n");
-            cargarPilaRandom(&pila, limite);
-            contarTitulo("PILA ANTES DE LA ORDENACION: ");
-            imprimirPila(pila);
-            apilarOrdenado(&pila);
-            contarTitulo("PILA DESPUES DE LA ORDENACION: ");
-            imprimirPila(pila);
-            break;
-
-        case 6:
-            contarTitulo("6. Hacer una funcion que inserta en una pila ordenada un nuevo elemento, conservando el orden de esta. \n");
-            cargarPilaRandom(&pila, limite);
-            insertarElementoEnOrdenada(&pila);
-            imprimirPila(pila);
-            break;
-
-        case 7:
-            contarTitulo("7. Hacer una funcion que pase los elementos de una pila a otra, de manera que se genere una nueva pila ordenada.");
-            contarTitulo("Usar la funci贸n del ejercicio 6.  (Ordenamiento por insercion).");
-            cargarPilaRandom(&pila, limite);
-            contarTitulo("PILA -PILA-: ");
-            imprimirPila(pila);
-            pasarPilaDeMenorAMayor (&pila, &otra);
-            contarTitulo("PILA -OTRA-: ");
-            imprimirPila(otra);
-            pasarPila(&otra, &pila);
-            break;
-
-        case 8:
-            contarTitulo("8- Hacer una funcion que sume y retorne los dos primeros elementos de una pila (tope y anterior),");
-            contarTitulo(" sin alterar su contenido. ");
-            cargarPilaRandom(&pila, limite);
-            printf("\t\t\t\t\t\tPILA RANDOM: \n");
-            printf("\t\t\t\t\t\t");
-            imprimirPila(pila);
-            suma = sumarElementos (pila);
-            printf("\n \t\t\t\t\t\tLA SUMA DE LOS VALORES  es: %i \n", suma);
-            break;
-
-
-
-
-
-        default:
-            printf(" EJERCICIO NO DISPONILE.\n");
-
-        }
-        printf("QUIERE VER OTRO EJERCICIO?\n");
-        printf("CUALQ.TECLA: SI.\n");
-        printf("ESC: NO.\n");
-        opcion = getch();
-        system("cls");
-    }
-}
-
-void cargarPila (Pila *p)
-{
-    int valor;
-    int corte = 0;
-
-    while (corte != ESC)
-    {
-        printf("\n Ingrese un numero: \n");
-        scanf("%i",&valor);
-        apilar(p, valor);
-        printf(" Desea seguir ingresando datos?\n Si: Cualquier tecla. \n No: ESC.");
-        corte = getch();
-        system("cls");
-    }
-}
-
-void imprimirPila(Pila p)
-{
-    int superior = 0;
-    Pila aux;
-    inicpila(&aux);
-    pasarPila(&p, &aux);
-    while(!pilavacia(&aux))
-    {
-        superior = tope(&aux);
-        printf("|%i|", superior);
-        apilar(&p, desapilar(&aux));
-    }
-    printf("\n");
-}
-
-void pasarPila (Pila *p, Pila *p2)
-{
-    while(!pilavacia(p))
-    {
-        apilar(p2, desapilar(p));
-    }
-}
-
-void pasarPilaIgual (Pila *p, Pila *p2)
-{
-    pasarPila (p, p2);
-    pasarPila (p2, p);
-
-}
-
-int encontrarMenor (Pila *p)
-{
-    Pila aux, menor;
-    inicpila(&menor);
-    inicpila(&aux);
-    int valor;
-
-    if(!pilavacia(p))
-    {
-        apilar(&menor, desapilar(p));
-        while(!pilavacia(p))
-        {
-            if(tope(p) < tope(&menor))
-            {
-                apilar(&aux, desapilar(&menor));
-                apilar(&menor, desapilar(p));
-            }
-            else
-            {
-                apilar(&aux, desapilar(p));
-            }
-        }
-
-    }
-    if(!pilavacia(&menor))
-    {
-        valor = tope(&menor);
-
-    }
-    while (!pilavacia(&aux))
-    {
-        apilar(p, desapilar(&aux));
-    }
-
-    return valor;
-
-}
-
-void apilarOrdenado(Pila *p)
-
-{
-    Pila aux, menor;
-    inicpila(&menor);
-    inicpila(&aux);
-
-    while(!pilavacia(p))
-    {
-        apilar(&menor, desapilar(p));
-        while(!pilavacia(p))
-        {
-            if(tope(p) < tope(&menor))
-            {
-                apilar(&aux, desapilar(&menor));
-                apilar(&menor, desapilar(p));
-            }
-            else
-            {
-                apilar(&aux, desapilar(p));
-            }
-        }
-
-        if(pilavacia(p))
-        {
-            pasarPila(&aux, p);
-        }
-    }
-    pasarPila(&menor, &aux);
-    pasarPila(&aux, p);
-}
-
-
-
-void contarTitulo(char titulo[])
-{
-    int anchoTotal = 118;
-    char* largo = titulo;
-    int contador = 0;
-    while (largo[contador] != 0)
-    {
-        contador ++;
-    }
-    int espacio = anchoTotal - contador;
-
-    printf("||");
-    for(int i = 0; i < anchoTotal-4; i++)
-    {
-        printf(" ");
-    }
-    printf("||");
-    printf("\n");
-    printf("||");
-    for(int i = 0; i < (espacio/2)-2; i++)
-    {
-        printf(" ");
-    }
-
-    printf("%s", titulo);
-
-
-    if(espacio % 2 != 0)
-    {
-        for(int i = 0; i < (espacio/2)-1; i++)
-        {
-            printf(" ");
-        }
+     system("color 27");
+     printf("El caracter SI se encuentra en el arreglo.");
     }
     else
     {
-        for(int i = 0; i < (espacio/2)-2; i++)
+        system("color 47");
+        printf("El caracter NO se encuentra en el arreglo.");
+    }
+
+    system("pause");
+    system("cls");
+    system("color 07");
+
+    printf("\n Indique que caracter quiere insertar en el arreglo: ");
+    fflush(stdin);
+    scanf("%c", &buscar);
+    system("cls");
+
+    validos = 0;
+    validos = cargarArregloCaracter (arrayCaracteres, validos);
+    validos = insertarOrdenado(arrayCaracteres, validos, buscar);
+    mostrarChar (arrayCaracteres, validos);
+    system("pause");
+
+    validos = 0;
+    validos = cargarArregloCaracter (otroArray, validos);
+    mayor = encontrarMayor (otroArray, validos);
+    mostrarChar (otroArray, validos);
+    printf("El caracter mayor es: %c", mayor);
+
+    return 0;
+}
+
+int cargarArreglo (int arreglo[], int valid)
+{
+    int opcion = 0;
+    printf("1- Hacer una funci髇 que reciba como par醡etro un arreglo de n鷐eros enteros y permita que el usuario ingrese valores al mismo por teclado. La funci髇 debe retornar la cantidad de elementos cargados en el arreglo (o pueden utilizar como puntero v醠idos)\n");
+    while (opcion != ESC &&  valid < DIM)
+    {
+        printf("Ingrese in valor al arreglo: \n");
+        scanf("%i", &arreglo[valid]);
+        valid ++;
+        printf("Seguir cargando datos?\n Cualq. tecla: SI\n Esc: NO");
+        opcion = getch();
+        system("cls");
+    }
+    return valid;
+}
+
+void mostrarArreglo (int arreglo[], int valid)
+{
+    int i = 0;
+    printf("2- Hacer una funci髇 que reciba como par醡etro un arreglo y la cantidad de elementos (v醠idos) cargados en 閘 y los muestre por pantalla.\n");
+    while (i < valid )
+    {
+        printf("|%i|", arreglo[i]);
+        i++;
+    }
+}
+
+void sumaElementos (int arreglo[], int valid)
+{
+    printf("\n 3- Hacer una funci髇 que reciba como par醡etro un arreglo, la cantidad de elementos (v醠idos) cargados en 閘 y una Pila. La funci髇 debe copiar los elementos del arreglo en la pila\n");
+    int total = 0;
+
+    for (int i = 0; i < valid; i++)
+    {
+        total = total + arreglo[i];
+    }
+    printf("la suma de todos los elementos del arreglo es: %i\n", total);
+}
+
+void copiarElementos(int arreglo[], int valid, Pila *p)
+{
+    printf("\n 4- Hacer una funci髇 que reciba como par醡etro un arreglo, la cantidad de elementos (v醠idos) cargados en 閘 y una Pila. La funci髇 debe copiar los elementos del arreglo en la pila.");
+    int i = 0;
+
+    while (i < valid)
+    {
+        apilar(p, arreglo[i]);
+        i++;
+    }
+    mostrar(p);
+
+}
+
+void sumaElementosFloat (float arreglo [], int valid)
+{
+    printf("\n 5- Realizar una funci髇 que sume los elementos de un arreglo de n鷐eros reales (float) de dimensi髇 100. (se recomienda hacer una funci髇 para cargar y otra para mostrar para este tipo de dato asociado al arreglo)");
+}
+
+void mostrarFloat (float arreglo[], int valid)
+{
+    int i = 0;
+    while (i < valid )
+    {
+        printf("|%.2f|", arreglo[i]);
+        i++;
+    }
+}
+
+int  cargarRandom(float arreglo[], int dimension, int valid)
+{
+
+    float valorRandom;
+    int i = 0;
+
+    for (i = 0; i < dimension; i++)
+    {
+        valorRandom = rand()%10 / 1.3 ;
+        arreglo[i] = valorRandom;
+
+    }
+    return i;
+}
+
+/// Realizar una funci髇 que indique si un elemento dado se encuentra en un arreglo de caracteres.
+
+int encontrarCaracter (char arreglo[], int valid, char caracter)
+{
+    int flag = 0;
+    int i = 0;
+
+    while(i < valid && flag == 0)
+    {
+        if (arreglo[i] == caracter)
         {
-            printf(" ");
+            flag = 1;
         }
-
+        i ++;
     }
-
-    printf("||");
-    printf("\n");
-    printf("||");
-    for(int i = 0; i < anchoTotal-4; i++)
-    {
-        printf(" ");
-    }
-    printf("||");
-    printf("\n");
+   return flag;
 }
 
-void imprimirTecho(int longitud)
+int cargarArregloCaracter (char arreglo[], int valid)
 {
-
-    for(int i = 0; i < longitud; i++)
+    int opcion = 0;
+    system("cls");
+    while (opcion != ESC &&  valid < DIM)
     {
-        printf("=");
+        printf("\n Ingrese un valor al arreglo de caracteres: \n");
+        fflush(stdin);
+        scanf("%c", &arreglo[valid]);
+        valid ++;
+        printf("Seguir cargando datos?\n Cualq. tecla: SI\n Esc: NO");
+        opcion = getch();
+        system("cls");
     }
-    printf("\n");
+    return valid;
 }
 
-
-
-
-void insertarElementoEnOrdenada(Pila *p)
+int insertarOrdenado(char a[], int v, char caracter)
 {
-    int valor;
+    int i = v - 1;
+    while(i >= 0 && a[i] > caracter){
+        a[i+1]=a[i];
+        i--;
+    }
+    a[i+1]=caracter;
 
-    printf("\n Ingrese un numero: \n");
-    scanf("%i",&valor);
-    apilar(p, valor);
-    apilarOrdenado(p);
+    return v+1;
 }
 
-void cargarPilaRandom(Pila *p, int lim)
+void mostrarChar (char arreglo[], int valid)
 {
-    if(pilavacia(p))
+    int i = 0;
+    while (i < valid )
     {
-        for(int i = 0; i < lim; i ++)
+        printf("|%c|", arreglo[i]);
+        i++;
+    }
+}
+
+
+///Realizar una funci髇 que obtenga el m醲imo car醕ter de un arreglo dado.
+
+char encontrarMayor (char a[], int valid)
+{
+    int i = valid -1;
+    char m = a[i];
+
+    while (i >= 0)
+    {
+        if (m < a[i] )
         {
-            apilar(p, rand()%(11-2) + 2);
+            m = a[i];
+            i--;
         }
-    }
-}
-
-void pasarPilaDeMenorAMayor (Pila *p, Pila *p2)
-{
-    Pila aux;
-    inicpila(&aux);
-    pasarPila(p,p2);
-    apilarOrdenado(p2);
-}
-
-int sumarElementos (Pila p)
-{
-    int total = 0,valor1 = 0, valor2 = 0;
-    Pila aux;
-    inicpila(&aux);
-
-    if(!pilavacia(&p))
-    {
-        valor1 = tope(&p);
-        apilar(&aux, desapilar(&p));
-
-        if(!pilavacia(&p))
+        else
         {
-            valor2 = tope(&p);
-            apilar(&aux, desapilar(&p));
+            i--;
         }
-        total = valor1 + valor2;
     }
-    return total;
-}
+    printf("ESTO VALE M: %c", m);
 
+    return m;
+}
 
